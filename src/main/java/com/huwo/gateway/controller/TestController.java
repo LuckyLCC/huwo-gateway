45a3f834b6ab1859436e8f710e05fe5c4cba4946
liuchang <liuchang061416!@#>
2022-10-20 18:01:18 +0800

    修改

--- src/main/java/com/huwo/gateway/controller/TestController.java
+++ src/main/java/com/huwo/gateway/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.huwo.gateway.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.huwo.gateway.domain.UpstreamBaseConfig;
 import com.huwo.gateway.listener.UpstreamBaseConfigSubscribe;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,7 +35,18 @@ public class TestController {
     @GetMapping("/kafka/test")
     public void sendMessage(@RequestBody String msg) {
         System.out.println("kaishi");
-        kafkaTemplate.send("orders-flume-new", msg);
+        ListenableFuture future = kafkaTemplate.send("orders-flume-new", msg);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.error("数据发送失败，失败数据为{}", msg);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> stringStringSendResult) {
+                log.info("数据发送成功,成功数据为{}",msg);
+            }
+        });
 //        kafkaTemplate.send("callmeappgps", msg);
     }
 
